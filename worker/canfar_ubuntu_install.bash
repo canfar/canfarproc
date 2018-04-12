298bf6094694d2f447ff4f0a3fcdbdd18c59e9f2
Sébastien Fabbro <sebfabbro@gmail.com>
2018-04-12 13:23:46 -0700

    documented canfar worker ubuntu

---
+++ worker/canfar_ubuntu_install.bash
@@ -0,0 +1,18 @@
+##!/bin/bash
+
+# This script might turn into a Dockerfile some day
+
+apt update -y
+apt dist-upgrade -y
+
+# deps for vos stuff
+apt install python-pip
+pip install vos vofs
+
+curl -sL https://github.com/canfar/canfarproc/raw/master/worker/bin/canfar_update -o /usr/local/bin/canfar_update
+chmod +x /usr/local/bin/canfar_update
+
+canfar_update
+
+# cleanup
+apt clean -y
