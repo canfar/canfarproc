cb87da8710e801b7aebf33a60080f9f845735c3e
Sébastien Fabbro <sebfabbro@gmail.com>
2018-04-12 10:03:19 -0700

    documenting a canfar centos setup for future containerization

---
+++ worker/canfar_centos_install.bash
@@ -0,0 +1,25 @@
+##!/bin/bash
+
+# This script might turn into a Dockerfile some day
+
+
+# bash love
+yum install bash-completion
+
+# add EPEL repo for PIP
+yum install -y epel-release
+yum update -y
+# deps for vos stuff
+yum install python-pip fuse-libs idna
+pip install vos vofs
+
+curl -sL https://github.com/canfar/canfarproc/raw/master/worker/bin/canfar_update -o /usr/local/bin/canfar_update
+chmod +x /usr/local/bin/canfar_update
+
+# default centos does not have /usr/local/bin in path when sudo'ing
+sed -e '/Defaults/s|:/usr/bin:/usr/local/bin:/usr/bin:' -i /etc/sudoers
+
+canfar_update
+
+# cleanup
+yum clean all -y
