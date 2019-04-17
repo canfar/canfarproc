5de5434e83fb9c87b04a550d1865e32005702cea
Sebastien Fabbro <sfabbro@uvic.ca>
2019-04-17 22:21:16 +0000

    modifications for ubuntu-18

--- worker/canfar_ubuntu_install.bash
+++ worker/canfar_ubuntu_install.bash
@@ -4,7 +4,13 @@
 
 apt update -y
 apt dist-upgrade -y
-apt install python-pip
+
+# python3 by default for 18.04 and above
+if [[ $(lsb_release -sr | sed -e 's|\.||') -ge 1804 ]]; then
+    apt install python3-pip -y
+else
+    apt install python-pip -y
+fi
 
 curl -sL https://github.com/canfar/canfarproc/raw/master/worker/bin/canfar_update -o /usr/local/bin/canfar_update
 chmod +x /usr/local/bin/canfar_update
