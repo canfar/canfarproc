4bbbc4f40165ebac3670b42c783660415b287b94
Sebastien Fabbro <sfabbro@uvic.ca>
2019-05-15 19:51:30 +0000

    centos 7 updates with python-3

--- worker/canfar_centos7_install.bash
+++ worker/canfar_centos7_install.bash
@@ -3,13 +3,13 @@
 # This script might turn into a Dockerfile some day
 
 # bash love
-yum install bash-completion
+yum install -y bash-completion
 
 # add EPEL repo for PIP
 yum install -y epel-release
 yum update -y
 # deps for vos and cadc stuff
-yum install python-pip fuse-libs idna python-args
+yum install -y python36-pip fuse-libs idna python-args
 
 curl -sL https://github.com/canfar/canfarproc/raw/master/worker/bin/canfar_update -o /usr/local/bin/canfar_update
 chmod +x /usr/local/bin/canfar_update
