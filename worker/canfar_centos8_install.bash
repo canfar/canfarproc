3ad9775c3379b536e9a3dac180a7eac2e26cea20
Sebastien Fabbro <sfabbro@uvic.ca>
2020-03-10 15:41:04 +0000

    added centos8 installation script

---
+++ worker/canfar_centos8_install.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script might turn into a Dockerfile some day
+
+# add EPEL repo for PIP
+dnf install -y epel-release
+dnf update -y
+
+# bash love
+dnf install -y bash-completion
+
+# deps for vos and cadc stuff
+dnf install -y python3-pip python3-devel python3-wheel openssl-devel
+
+# stuff for pip and users might like to have fortran...
+dnf group install -y 'Development Tools'
+
+curl -sL https://github.com/canfar/canfarproc/raw/master/worker/bin/canfar_update -o /usr/local/bin/canfar_update
+chmod +x /usr/local/bin/canfar_update
+
+# default centos does not have /usr/local/bin in path when sudo'ing
+sed -e '/Defaults/s|:/usr/bin|:/usr/local/bin:/usr/bin:|' -i /etc/sudoers
+
+export PATH="/usr/local/bin:${PATH}"
+canfar_update
+
+# cleanup
+dnf clean all -y
